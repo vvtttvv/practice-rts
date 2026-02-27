27ccb92a91d70c2e62d8db2f04153501e54128b3
Мдфв <v.v.titerez@gmail.com>
2026-02-27 14:47:25 +0200

    Played with Console and Environment

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace  Task1;
+
+using System;
+
+class Program
+{
+    static void Main(string [ ] args)
+    {
+        ShowEnvironment.GetInfo();
+        var vas = Console.Read();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine(vas);
+        
+
+    }
+}
